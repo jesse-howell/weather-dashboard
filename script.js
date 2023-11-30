8275da30233c20f1aa819511a6ae9aa52f0ff336
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2023-11-30 18:33:21 -0500

    broke down input to flow read vertically, added two variables to js

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-var searchButton = document.getElementById("search-btn");
-
-async function getWeatherData(lat, lon) {
-    var apiKey = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-
-var response = await fetch("api.openweathermap.org/data/2.5/forecast?lat={35.327113512782006}&lon={-80.78882936595913}&appid={API key}");
-    return response.json();
-      };
-      
-
-searchButton.addEventListener("click", getWeatherData);
+var locSearch = document.querySelector("#loc-search");
+var qInput = document.querySelector("#q");
