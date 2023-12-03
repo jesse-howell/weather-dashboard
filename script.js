b11a2e9b5ed74f2d3646c534c42251e4430d036a
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2023-12-02 22:52:24 -0500

    added handleSearch for search bar

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function getGeoWeather(lat, lon) {
 // get city geo data
 function getGeoData() {
     fetch(
-      "https://api.openweathermap.org/geo/1.0/direct?appid=29f6ddfef096bce3ebb4f3b252bafe28&limit=1&q=Atlanta"
+      "https://api.openweathermap.org/geo/1.0/direct?appid=29f6ddfef096bce3ebb4f3b252bafe28&limit=1&q=Denver"
     )
       .then(function (response) {
         return response.json();
@@ -36,8 +36,22 @@ function getGeoData() {
 // for loop to be paired with button clicks
       const city = ["Atlanta", "Denver", "Seattle", "San Francisco", "Orlando", "New York", "Chicago", "Austin"];
       for (let i = 0; i < city.length; i++) {
-        console.log(city);
+        console.log(city + i++);
       }
   }
   // call geo data function
+
   getGeoData();
+// added handleSearch for search bar
+  function handleSearch(event) {
+    event.preventDefault();
+    const format = formatInput.value;
+    const q = qInput.value;
+    const type = "search";
+    if (format) {
+      type = format;
+    }
+    const url = "https://api.openweather.org" + type + "/?q=" + q + "&fo=json";
+    console.log("URL", url);
+
+  };
